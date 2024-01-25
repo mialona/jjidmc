9e6def001c2961fdccd26f0515ee8f5cbc45f0bb
mialona <mialona@unirioja.es>
2024-01-25 18:11:00 +0100

    Program differentiation

--- js/custom.js
+++ js/custom.js
@@ -6,17 +6,6 @@ $(window).load(function(){
 });
 
 
-/* Mobile Navigation
-    -----------------------------------------------*/
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
-
-
 /* HTML document is loaded. DOM is ready. 
 -------------------------------------------*/
 $(document).ready(function() {
